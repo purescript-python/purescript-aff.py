1680188c78b03f318fbc29553310912e72891f23
Mike Solomon <mike@meeshkan.com>
2020-06-28 21:46:11 +0300

    Moves test to Main

--- spago.dhall
+++ spago.dhall
@@ -3,8 +3,8 @@ Welcome to a Spago project!
 You can edit this file as you like.
 -}
 { name = "purescript-aff"
-, dependencies = [ "aff", "console", "effect", "psci-support" ]
+, dependencies = [ "aff", "console", "effect", "psci-support", "partial", "minibench", "assert", "free" ]
 , packages = ./packages.dhall
-, sources = [ "src/**/*.purs", "test/**/*.purs" ]
+, sources = [ "src/**/*.purs"]
 , backend = "pspy"
 }
--- src/Bench.purs
+++ src/Bench.purs
@@ -1,4 +1,4 @@
-module Test.Bench where
+module Bench where
 
 import Prelude
 import Control.Monad.Rec.Class (Step(..), tailRecM)
@@ -12,52 +12,52 @@ loop1 ∷ Int → Aff.Aff Int
 loop1 = tailRecM go
   where
   go n
-    | n <= 0    = pure $ Done n
+    | n <= 0 = pure $ Done n
     | otherwise = do
+      _ ← do
         _ ← do
-          _ ← do
-            _ ← pure n
-            _ ← pure n
-            _ ← pure n
-            pure n
           _ ← pure n
           _ ← pure n
           _ ← pure n
           pure n
-        pure $ Loop (n - 1)
+        _ ← pure n
+        _ ← pure n
+        _ ← pure n
+        pure n
+      pure $ Loop (n - 1)
 
 loop2 ∷ Int → Aff.Aff Int
 loop2 = go
   where
   go n
-    | n <= 0    = pure n
+    | n <= 0 = pure n
     | otherwise = do
+      _ ← do
         _ ← do
-          _ ← do
-            _ ← pure n
-            _ ← pure n
-            _ ← pure n
-            pure n
           _ ← pure n
           _ ← pure n
           _ ← pure n
           pure n
-        loop2 (n - 1)
+        _ ← pure n
+        _ ← pure n
+        _ ← pure n
+        pure n
+      loop2 (n - 1)
 
 fib1 ∷ Int → Aff.Aff Int
-fib1 n = if n <= 1 then pure n else do
-  a ← fib1 (n - 1)
-  b ← fib1 (n - 2)
-  pure (a + b)
+fib1 n =
+  if n <= 1 then
+    pure n
+  else do
+    a ← fib1 (n - 1)
+    b ← fib1 (n - 2)
+    pure (a + b)
 
 main ∷ Effect Unit
 main = do
   Console.log "\nAff tailRecM:"
   bench \_ → unsafePerformEffect $ void $ Aff.launchAff $ loop1 10000
-
   Console.log "\nAff loop:"
   bench \_ → unsafePerformEffect $ void $ Aff.launchAff $ loop2 10000
-
   Console.log "\nAff fib:"
   bench \_ → unsafePerformEffect $ void $ Aff.launchAff $ fib1 20
-
